03d6698008e39a4af4e848c6ef39a66aab2a4683
Dwi Aji Prasetyo <73772152+aprxty3@users.noreply.github.com>
2021-09-03 19:54:13 +0700

    Dart Basic

---
+++ Dart Basic/2.Variabel_DataType.dart
@@ -0,0 +1,17 @@
+void main() {
+  //Variabel
+
+  var name = 'Aji Prasetyo';
+  var year = 1998;
+  var hobby = ['Ngegame', 'Dengerin Music', 'Ngesambat'];
+  var image = {
+    'tags': ['Ngegame']
+  };
+
+  print(name);
+  print(year);
+  print(hobby);
+  print(image);
+
+  print('nama $name');
+}
---
+++ Dart Basic/3.Print&Comment.dart
@@ -0,0 +1,26 @@
+void main() {
+  //Variabel
+
+  var name = 'Aji Prasetyo';
+  var year = 1998;
+  var hobby = ['Ngegame', 'Dengerin Music', 'Ngesambat'];
+  var image = {
+    'tags': ['Ngegame']
+  };
+
+//Print
+  print(name);
+  print(year);
+  print(hobby);
+  print(image);
+
+  print('nama $name');
+  print(hobby[0]);
+  print('hobby favorite ${hobby[0]}');
+}
+
+/*
+ini namanya multi comment
+*/
+
+// ini namanya single comment
